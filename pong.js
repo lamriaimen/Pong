6b454bd38ac1e90935c5464ad1bf8d919f220ffc
MOHAMED SAID AIMEN LAMRI <lamrimohamedsaidaimen@gmail.com>
2023-02-04 18:24:34 +0100

    paddle modif

--- pong.js
+++ pong.js
@@ -11,7 +11,7 @@ this.width = ball.offsetWidth;
 
 }
 function Paddle(side) {
-    this.id = side == "left" ? "padle1" : "padle2";
+    this.id = side == "left" ? "paddle1" : "paddle2";
     this.x = side == "left" ? 0 : window.innerWidth - 24;
     this.y = window.innerHeight / 2 - 96;
     this.height = 192;
@@ -34,16 +34,36 @@ if (ball.x <= 0 || ball.x >= bodyRect.width - ball.height) {
 if (ball.y <=0 || ball.y >= bodyRect.height -ball.width) {
   ball.vy = -ball.vy;
 }
-place_objects([ball, padle1, padle2]);
+place_objects([ball, paddle1, paddle2]);
 }
 
+function track_player_input(event) {
+    if(event.type == "keydown") {
+    switch(event.key) {
+    case "a": buttons.p1_up = true; break;
+    case "q": buttons.p1_down = true; break;
+    case "p": buttons.p2_up = true; break;
+    case "m": buttons.p2_down = true; break;
+    }
+    } else if(event.type == "keyup") {
+    switch(event.key) {
+    case "a": buttons.p1_up = false; break;
+    case "q": buttons.p1_down = false; break;
+    case "p": buttons.p2_up = false; break;
+    case "m": buttons.p2_down = false; break;
+    }
+    }
+    
+    }
+    document.addEventListener("keydown", track_player_input);
+    document.addEventListener("keyup", track_player_input);
 
-let ball, padle1, padle2;
+let ball, paddle1, paddle2;
 function init() {
     ball = new Ball();
-    padle1 = new Paddle("left");
-    padle2 = new Paddle("right");
-    place_objects([ball, padle1, padle2]);
+    paddle1 = new Paddle("left");
+    paddle2 = new Paddle("right");
+    place_objects([ball, paddle1, paddle2]);
     setInterval(update, 100);
 }
 
