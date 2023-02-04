e8d3d6d09d53559250dbfba76ec4ed9cd1286a18
Zironics <rafikobouloudene@gmail.com>
2023-02-04 18:05:53 +0100

    6) not finished

--- pong.js
+++ pong.js
@@ -17,14 +17,52 @@ element.style.left = object.x + "px";
 element.style.top = object.y + "px";
 }
 }
+
 function update() {
-ball.x += 5;
-ball.y += 5;
+    ball.x += 5;
+    ball.y += 5;
+    for(let key in buttons) {
+        if(buttons[key]) {
+            switch (key) {
+                case "p1_up": padle1.y -= 3; break;
+                case "p1_down": padle1.y += 3; break;
+                case "p2_up": padle2.y -= 3; break;
+                case "p2_down": padle2.y += 3; break;
+            }
+        }
+    }
 place_objects([ball, padle1, padle2]);
 }
 
+function updatePadle(padle,up) {
+   if(up && padle.y+3)
+}
+
+function track_player_input(event) {
+    if (event.type == "keydown") {
+        switch (event.key) {
+            case "a": buttons.p1_up = true; break;
+            case "q": buttons.p1_down = true; break;
+            case "p": buttons.p2_up = true; break;
+            case "m": buttons.p2_down = true; break;
+        }
+    } else if (event.type == "keyup") {
+        switch (event.key) {
+            case "a": buttons.p1_up = false; break;
+            case "q": buttons.p1_down = false; break;
+            case "p": buttons.p2_up = false; break;
+            case "m": buttons.p2_down = false; break;
+        }
+    }
+}
 
 let ball, padle1, padle2;
+let buttons = {
+    "p1_up":false,
+    "p1_down":false,
+    "p2_up":false,
+    "p2_down": false,
+}
 function init() {
     ball = new Ball();
     padle1 = new Paddle("left");
@@ -33,5 +71,6 @@ function init() {
     setInterval(update, 100);
 }
 
-
+document.addEventListener("keydown", track_player_input);
+document.addEventListener("keyup", track_player_input);
 window.addEventListener("load", init);
